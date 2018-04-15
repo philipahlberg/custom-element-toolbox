674b4c0d3f13fe7415248894fad41af5cac92542
philipahlberg <philipahlberg@users.noreply.github.com>
2018-04-16 00:03:14 +0200

    add `debounce`, `once` and `cache` utility functions

--- src/utils.js
+++ src/utils.js
@@ -27,3 +27,48 @@ export function freeze(object) {
 }
 
 export const EMPTY = freeze(empty());
+
+const timeouts = new WeakMap();
+
+export function debounce(fn) {
+  const wrapper = () => {
+    fn();
+    timeouts.delete(fn);
+  }
+
+  return () => {
+    clearTimeout(timeouts.get(fn));
+    timeouts.set(fn, setTimeout(wrapper, 1));
+  }
+}
+
+const called = new WeakSet();
+
+export function once(fn) {
+  return () => {
+    if (!called.has(fn)) {
+      fn();
+      called.add(fn);
+    }
+  }
+}
+
+const input = new WeakMap();
+const output = new WeakMap();
+
+export function cache(fn) {
+  return (...args) => {
+    const previous = input.get(fn);
+    const equal = previous && args.every(
+      (item, i) => item === previous[i]
+    );
+    if (equal) {
+      return output.get(fn);
+    } else {
+      const result = fn(...args);
+      input.set(fn, args);
+      output.set(fn, result);
+      return result;
+    }
+  }
+}
