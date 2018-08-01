ada06f8bcae80b48b96c06999b6519ff48b25020
Philip Ahlberg <philipahlberg@users.noreply.github.com>
2018-08-01 13:03:51 +0200

    remove `toggleAttribute` test from AttributeMixin

--- test/attribute-mixin.spec.js
+++ test/attribute-mixin.spec.js
@@ -87,19 +87,4 @@ describe('AttributeMixin', () => {
     element.setAttribute('camel-case', 'anything');
     expect(element.camelCase).to.equal('anything');
   });
-
-  it('toggles an attribute', () => {
-    expect(element.hasAttribute('attribute')).to.be.false;
-    element.toggleAttribute('attribute');
-    expect(element.hasAttribute('attribute')).to.be.true;
-    element.toggleAttribute('attribute');
-    expect(element.hasAttribute('attribute')).to.be.false;
-  });
-
-  it('conditionally toggles an attribute', () => {
-    element.toggleAttribute('attribute', true);
-    expect(element.hasAttribute('attribute')).to.be.true;
-    element.toggleAttribute('attribute', false);
-    expect(element.hasAttribute('attribute')).to.be.false;
-  });
 });
