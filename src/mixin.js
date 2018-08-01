d159b50e400d92c487be66d9d4b2ca104e6b1040
Philip Ahlberg <philipahlberg@users.noreply.github.com>
2018-08-01 12:58:59 +0200

    create Mixin
    Mixin is a mixin creator function that deduplicates mixins so that a
    single mixin is only ever applied once in the prototype chain.

---
+++ src/mixin.js
@@ -0,0 +1,18 @@
+export const Mixin = Mix => {
+  const mixes = new WeakSet();
+
+  return SuperClass => {
+    let prototype = SuperClass;
+    while (prototype != null) {
+      if (mixes.has(prototype)) {
+        return SuperClass;
+      }
+
+      prototype = Object.getPrototypeOf(prototype);
+    }
+
+    const application = Mix(SuperClass);
+    mixes.add(application);
+    return application;
+  }
+};
