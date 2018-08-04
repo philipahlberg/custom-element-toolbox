6add020acc948414438be12e2df69e8528c9f1b1
Philip Ahlberg <philipahlberg@users.noreply.github.com>
2018-08-04 22:48:18 +0200

    remove unused emit method

--- src/base-mixin.js
+++ src/base-mixin.js
@@ -3,23 +3,6 @@ import { Mixin } from './mixin.js';
 export const BaseMixin = Mixin(SuperClass => {
   return class BaseElement extends SuperClass {
     /**
-     * Shorthand for emitting a custom event.
-     * 
-     * @param {string} type The type/name of the event.
-     * @param {*} detail Any extra state to include in the event.
-     * @param {CustomEventInit} options
-     */
-    emit(type, detail, options) {
-      const defaults = {
-        bubbles: true,
-        cancelable: true,
-        detail
-      };
-      const init = Object.assign(defaults, options);
-      return this.dispatchEvent(new CustomEvent(type, init));
-    }
-
-    /**
      * Toggle a boolean attribute (removing it if it is present
      * and adding it if it is not present) on the specified element.
      * 
