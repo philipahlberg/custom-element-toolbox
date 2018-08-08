de14698d4fbbf29a4d06b8a5fa6345d3db688b1b
Philip Ahlberg <philipahlberg@users.noreply.github.com>
2018-08-08 12:42:07 +0200

    update exports

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-export { AttributesMixin } from './attributes-mixin.js';
 export { BaseMixin } from './base-mixin.js';
 export { connector } from './connector.js';
 export { ControlMixin } from './control-mixin.js';
 export { FocusMixin } from './focus-mixin.js';
 export { PropertiesMixin } from './properties-mixin.js';
+export { PropertyChangedMixin } from './property-changed-mixin.js';
+export { PropertyObserverMixin } from './property-observer-mixin.js';
+export { PropertyReflectionMixin } from './property-reflection-mixin.js';
 export { ShadyTemplateMixin } from './shady-template-mixin.js';
 export { StaticTemplateMixin, html } from './static-template-mixin.js';
 export { ToggleMixin } from './toggle-mixin.js';
