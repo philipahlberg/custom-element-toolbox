9bb70410ae60eb85b61f0ef3d6d7bd82f50ae149
Philip Ahlberg <philipahlberg@users.noreply.github.com>
2018-08-28 13:30:44 +0200

    test: AttributeDeserialization

---
+++ test/attribute-deserialization.spec.js
@@ -0,0 +1,75 @@
+import { AttributeDeserialization } from '../dist/index.js';
+
+const Base = AttributeDeserialization(HTMLElement);
+
+class AttributeElement extends Base {
+  static get properties() {
+    return {
+      string: {
+        type: String
+      },
+      number: {
+        type: Number
+      },
+      boolean: {
+        type: Boolean
+      },
+      camelCase: {
+        type: String
+      },
+      reflected: {
+        type: String,
+        reflectToAttribute: true
+      }
+    }
+  }
+}
+
+customElements.define('attribute-element', AttributeElement);
+
+describe('AttributeDeserialization', () => {
+  let element;
+
+  beforeEach(() => {
+    element = new AttributeElement();
+    document.body.appendChild(element);
+  });
+
+  afterEach(() => {
+    document.body.removeChild(element);
+  });
+
+  it('does not read values when there are no attributes', () => {
+    expect(element.string).to.equal(undefined);
+    expect(element.number).to.equal(undefined);
+    expect(element.boolean).to.equal(undefined);
+    expect(element.camelCase).to.equal(undefined);
+    expect(element.reflected).to.equal(undefined);
+  });
+
+  it('deserializes strings', () => {
+    element.setAttribute('string', 'value');
+    expect(element.string).to.equal('value');
+  });
+
+  it('deserializes numbers', () => {
+    element.setAttribute('number', '10');
+    expect(element.number).to.equal(10);
+  });
+
+  it('deserializes booleans', () => {
+    element.setAttribute('boolean', '');
+    expect(element.boolean).to.equal(true);
+  });
+
+  it('converts dash-case to camelCase', () => {
+    element.setAttribute('camel-case', 'foo');
+    expect(element.camelCase).to.equal('foo');
+  });
+
+  it('does not deserialize property reflections', () => {
+    expect(() => {
+      element.reflected = 'foo';
+    }).to.not.throw();
+  });
+});
