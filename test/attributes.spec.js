aa765e3fde631e401b9e3eb5d597b0b0e3eee2a5
Philip Ahlberg <philipahlberg@users.noreply.github.com>
2018-09-04 13:23:04 +0200

    Add Properties interop tests

--- test/attributes.spec.js
+++ test/attributes.spec.js
@@ -1,75 +1,151 @@
-import { Attributes } from '../dist/index.js';
-
-const Base = Attributes(HTMLElement);
-
-class AttributesElement extends Base {
-  static get properties() {
-    return {
-      string: {
-        type: String
-      },
-      number: {
-        type: Number
-      },
-      boolean: {
-        type: Boolean
-      },
-      camelCase: {
-        type: String
-      },
-      reflected: {
-        type: String,
-        reflectToAttribute: true
-      }
-    }
-  }
-}
-
-customElements.define('attributes-element', AttributesElement);
+import { Attributes, Properties } from '../dist/index.js';
 
 describe('Attributes', () => {
-  let element;
+  describe('Standalone', () => {
+    const Super = Attributes(HTMLElement);
 
-  beforeEach(() => {
-    element = new AttributesElement();
-    document.body.appendChild(element);
+    class AttributesElement extends Super {
+      static get properties() {
+        return {
+          string: {
+            type: String
+          },
+          number: {
+            type: Number
+          },
+          boolean: {
+            type: Boolean
+          },
+          camelCase: {
+            type: String
+          },
+          reflected: {
+            type: String,
+            reflectToAttribute: true
+          }
+        }
+      }
+    }
+
+    customElements.define('attributes-element', AttributesElement);
+    
+    let element;
+  
+    beforeEach(() => {
+      element = new AttributesElement();
+      document.body.appendChild(element);
+    });
+  
+    afterEach(() => {
+      document.body.removeChild(element);
+    });
+  
+    it('does not read values when there are no attributes', () => {
+      expect(element.string).to.equal(undefined);
+      expect(element.number).to.equal(undefined);
+      expect(element.boolean).to.equal(undefined);
+      expect(element.camelCase).to.equal(undefined);
+      expect(element.reflected).to.equal(undefined);
+    });
+  
+    it('deserializes strings', () => {
+      element.setAttribute('string', 'value');
+      expect(element.string).to.equal('value');
+    });
+  
+    it('deserializes numbers', () => {
+      element.setAttribute('number', '10');
+      expect(element.number).to.equal(10);
+    });
+  
+    it('deserializes booleans', () => {
+      element.setAttribute('boolean', '');
+      expect(element.boolean).to.equal(true);
+    });
+  
+    it('converts dash-case to camelCase', () => {
+      element.setAttribute('camel-case', 'foo');
+      expect(element.camelCase).to.equal('foo');
+    });
+  
+    it('does not deserialize property reflections', () => {
+      expect(() => {
+        element.reflected = 'foo';
+      }).to.not.throw();
+    });
   });
 
-  afterEach(() => {
-    document.body.removeChild(element);
-  });
+  describe('+ Properties', () => {
+    const Super = Attributes(Properties(HTMLElement));
 
-  it('does not read values when there are no attributes', () => {
-    expect(element.string).to.equal(undefined);
-    expect(element.number).to.equal(undefined);
-    expect(element.boolean).to.equal(undefined);
-    expect(element.camelCase).to.equal(undefined);
-    expect(element.reflected).to.equal(undefined);
-  });
+    class MixedElement extends Super {
+      static get properties() {
+        return {
+          string: {
+            type: String
+          },
+          number: {
+            type: Number
+          },
+          boolean: {
+            type: Boolean
+          },
+          camelCase: {
+            type: String
+          },
+          reflected: {
+            type: String,
+            reflectToAttribute: true
+          }
+        }
+      }
+    }
 
-  it('deserializes strings', () => {
-    element.setAttribute('string', 'value');
-    expect(element.string).to.equal('value');
-  });
+    customElements.define('attributes-mixed-element', MixedElement);
 
-  it('deserializes numbers', () => {
-    element.setAttribute('number', '10');
-    expect(element.number).to.equal(10);
-  });
-
-  it('deserializes booleans', () => {
-    element.setAttribute('boolean', '');
-    expect(element.boolean).to.equal(true);
-  });
-
-  it('converts dash-case to camelCase', () => {
-    element.setAttribute('camel-case', 'foo');
-    expect(element.camelCase).to.equal('foo');
-  });
-
-  it('does not deserialize property reflections', () => {
-    expect(() => {
-      element.reflected = 'foo';
-    }).to.not.throw();
-  });
+    let element;
+  
+    beforeEach(() => {
+      element = new MixedElement();
+      document.body.appendChild(element);
+    });
+  
+    afterEach(() => {
+      document.body.removeChild(element);
+    });
+  
+    it('does not read values when there are no attributes', () => {
+      expect(element.string).to.equal(undefined);
+      expect(element.number).to.equal(undefined);
+      expect(element.boolean).to.equal(undefined);
+      expect(element.camelCase).to.equal(undefined);
+      expect(element.reflected).to.equal(undefined);
+    });
+  
+    it('deserializes strings', () => {
+      element.setAttribute('string', 'value');
+      expect(element.string).to.equal('value');
+    });
+  
+    it('deserializes numbers', () => {
+      element.setAttribute('number', '10');
+      expect(element.number).to.equal(10);
+    });
+  
+    it('deserializes booleans', () => {
+      element.setAttribute('boolean', '');
+      expect(element.boolean).to.equal(true);
+    });
+  
+    it('converts dash-case to camelCase', () => {
+      element.setAttribute('camel-case', 'foo');
+      expect(element.camelCase).to.equal('foo');
+    });
+  
+    it('does not deserialize property reflections', () => {
+      expect(() => {
+        element.reflected = 'foo';
+      }).to.not.throw();
+    });
+  })
 });
