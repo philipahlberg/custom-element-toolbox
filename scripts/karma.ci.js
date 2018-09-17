9caf7217d156f5ae9b65dc511b9f38addd955c8f
Philip Ahlberg <philipahlberg@users.noreply.github.com>
2018-09-17 13:49:00 +0200

    add --no-sandbox when launching Chrome in CI

---
+++ scripts/karma.ci.js
@@ -0,0 +1,42 @@
+const polyfills = [
+  '@webcomponents/custom-elements',
+  '@webcomponents/shadydom',
+  '@webcomponents/shadycss/scoping-shim.min.js'
+].map(p => ({ pattern: require.resolve(p) }));
+
+module.exports = (config) => {
+  config.set({
+    basePath: '../',
+    frameworks: ['mocha', 'chai', 'sinon'],
+    files: [
+      ...polyfills,
+      { pattern: 'dist/index.js', type: 'module' },
+      { pattern: 'test/*.js', type: 'module' }
+    ],
+    browsers: ['ChromeCI', 'FirefoxCI'],
+    customLaunchers: {
+      ChromeCI: {
+        base: 'Chrome',
+        flags: [
+          '--headless',
+          '--no-sandbox'
+        ]
+      },
+      FirefoxCI: {
+        base: 'Firefox',
+        flags: [
+          '-headless'
+        ],
+      },
+    },
+    reporters: ['progress'],
+    port: 1234,
+    colors: true,
+    logLevel: config.LOG_WARN,
+    autoWatch: false,
+    singleRun: true,
+    // https://github.com/karma-runner/karma/pull/2834#issuecomment-376854730
+    customContextFile: 'test/context.html',
+    customDebugFile: 'test/debug.html'
+  });
+};
